aac07207d7306b777bab5880e76a5effbb817119
oruiz622 <oruiz622@gmail.com>
2019-04-13 19:43:16 -0400

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: 'AIzaSyDWIwH8kzBoSXxt_zk7Zzj6xESu8X8Sa6A',
+		authDomain: 'clientpanelprod-1511c.firebaseapp.com',
+		databaseURL: 'https://clientpanelprod-1511c.firebaseio.com',
+		projectId: 'clientpanelprod-1511c',
+		storageBucket: 'clientpanelprod-1511c.appspot.com',
+		messagingSenderId: '867484842286'
+	}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firebase: {
+		apiKey: 'AIzaSyDWIwH8kzBoSXxt_zk7Zzj6xESu8X8Sa6A',
+		authDomain: 'clientpanelprod-1511c.firebaseapp.com',
+		databaseURL: 'https://clientpanelprod-1511c.firebaseio.com',
+		projectId: 'clientpanelprod-1511c',
+		storageBucket: 'clientpanelprod-1511c.appspot.com',
+		messagingSenderId: '867484842286'
+	}
 };
 
 /*
